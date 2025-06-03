600591d1c3525d84b341e7977779179ffdc62322
Seth Cooper <seth.cooper@gmail.com>
2025-06-03 16:22:46 -0400

    Update soko example.

--- _run_soko_diff.sh
+++ _run_soko_diff.sh
@@ -8,47 +8,47 @@ count="$1"
 
 # make transformed and concatenated levels
 
-python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_00_7x.lvl --pad-between 2 --game 0 1 2 X --jsonfile stwfc/AIIDE/training/soko/soko_1.json
-python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_01_7x.lvl --pad-between 2 --game 0 1 2 X --jsonfile stwfc/AIIDE/training/soko/soko_1.json --xform-flip-rows
-python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_02_7x.lvl --pad-between 2 --game 0 1 2 X --jsonfile stwfc/AIIDE/training/soko/soko_1.json --xform-flip-cols
-python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_03_7x.lvl --pad-between 2 --game 0 1 2 X --jsonfile stwfc/AIIDE/training/soko/soko_1.json --xform-flip-rows --xform-flip-cols
+python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_00_11x.lvl --pad-between 2 --game 0 1 2 X --jsonfile setup/soko_og.json
+python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_01_11x.lvl --pad-between 2 --game 0 1 2 X --jsonfile setup/soko_og.json --xform-flip-rows
+python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_02_11x.lvl --pad-between 2 --game 0 1 2 X --jsonfile setup/soko_og.json --xform-flip-cols
+python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_03_11x.lvl --pad-between 2 --game 0 1 2 X --jsonfile setup/soko_og.json --xform-flip-rows --xform-flip-cols
 
-python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_04_6x.lvl --pad-between 2 --game 0 1 2 X --jsonfile stwfc/AIIDE/training/soko/soko_1.json --xform-rotate 1
-python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_05_6x.lvl --pad-between 2 --game 0 1 2 X --jsonfile stwfc/AIIDE/training/soko/soko_1.json --xform-rotate 1 --xform-flip-rows
-python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_06_6x.lvl --pad-between 2 --game 0 1 2 X --jsonfile stwfc/AIIDE/training/soko/soko_1.json --xform-rotate 1 --xform-flip-cols
-python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_07_6x.lvl --pad-between 2 --game 0 1 2 X --jsonfile stwfc/AIIDE/training/soko/soko_1.json --xform-rotate 1 --xform-flip-rows --xform-flip-cols
+python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_04_19x.lvl --pad-between 2 --game 0 1 2 X --jsonfile setup/soko_og.json --xform-rotate 1
+python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_05_19x.lvl --pad-between 2 --game 0 1 2 X --jsonfile setup/soko_og.json --xform-rotate 1 --xform-flip-rows
+python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_06_19x.lvl --pad-between 2 --game 0 1 2 X --jsonfile setup/soko_og.json --xform-rotate 1 --xform-flip-cols
+python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_07_19x.lvl --pad-between 2 --game 0 1 2 X --jsonfile setup/soko_og.json --xform-rotate 1 --xform-flip-rows --xform-flip-cols
 
-python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_08_7x.lvl --pad-between 2 --game 0 1 2 X --jsonfile stwfc/AIIDE/training/soko/soko_1.json --xform-rotate 2
-python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_09_7x.lvl --pad-between 2 --game 0 1 2 X --jsonfile stwfc/AIIDE/training/soko/soko_1.json --xform-rotate 2 --xform-flip-rows
-python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_10_7x.lvl --pad-between 2 --game 0 1 2 X --jsonfile stwfc/AIIDE/training/soko/soko_1.json --xform-rotate 2 --xform-flip-cols
-python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_11_7x.lvl --pad-between 2 --game 0 1 2 X --jsonfile stwfc/AIIDE/training/soko/soko_1.json --xform-rotate 2 --xform-flip-rows --xform-flip-cols
+python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_08_11x.lvl --pad-between 2 --game 0 1 2 X --jsonfile setup/soko_og.json --xform-rotate 2
+python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_09_11x.lvl --pad-between 2 --game 0 1 2 X --jsonfile setup/soko_og.json --xform-rotate 2 --xform-flip-rows
+python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_10_11x.lvl --pad-between 2 --game 0 1 2 X --jsonfile setup/soko_og.json --xform-rotate 2 --xform-flip-cols
+python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_11_11x.lvl --pad-between 2 --game 0 1 2 X --jsonfile setup/soko_og.json --xform-rotate 2 --xform-flip-rows --xform-flip-cols
 
-python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_12_6x.lvl --pad-between 2 --game 0 1 2 X --jsonfile stwfc/AIIDE/training/soko/soko_1.json --xform-rotate 3
-python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_13_6x.lvl --pad-between 2 --game 0 1 2 X --jsonfile stwfc/AIIDE/training/soko/soko_1.json --xform-rotate 3 --xform-flip-rows
-python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_14_6x.lvl --pad-between 2 --game 0 1 2 X --jsonfile stwfc/AIIDE/training/soko/soko_1.json --xform-rotate 3 --xform-flip-cols
-python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_15_6x.lvl --pad-between 2 --game 0 1 2 X --jsonfile stwfc/AIIDE/training/soko/soko_1.json --xform-rotate 3 --xform-flip-rows --xform-flip-cols
+python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_12_19x.lvl --pad-between 2 --game 0 1 2 X --jsonfile setup/soko_og.json --xform-rotate 3
+python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_13_19x.lvl --pad-between 2 --game 0 1 2 X --jsonfile setup/soko_og.json --xform-rotate 3 --xform-flip-rows
+python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_14_19x.lvl --pad-between 2 --game 0 1 2 X --jsonfile setup/soko_og.json --xform-rotate 3 --xform-flip-cols
+python sturgeon/level2concat.py --outfile _out/run/soko/diff/in_15_19x.lvl --pad-between 2 --game 0 1 2 X --jsonfile setup/soko_og.json --xform-rotate 3 --xform-flip-rows --xform-flip-cols
 
 # get tileset
 
-python sturgeon/input2tile.py --outfile _out/run/soko/diff/setup_ts.tileset --out-tileset --textfile setup/soko-tiles.lvl --imagefile sturgeon/levels/kenney/soko-tile-16.png
+python sturgeon/input2tile.py --outfile _out/run/soko/diff/setup_ts.tileset --out-tileset --textfile setup/soko_og-tiles.lvl --imagefile sturgeon/levels/kenney/soko-over-tile-16.png
 
 # make tile and scheme files for each height
 
-python sturgeon/input2tile.py --outfile _out/run/soko/diff/setup_6x.tile --textfile _out/run/soko/diff/in_*_6x.lvl --gamefile _out/run/soko/diff/in_*_6x.game --tileset _out/run/soko/diff/setup_ts.tileset --text-key-only
-python sturgeon/input2tile.py --outfile _out/run/soko/diff/setup_7x.tile --textfile _out/run/soko/diff/in_*_7x.lvl --gamefile _out/run/soko/diff/in_*_7x.game --tileset _out/run/soko/diff/setup_ts.tileset --text-key-only
+python sturgeon/input2tile.py --outfile _out/run/soko/diff/setup_11x.tile --textfile _out/run/soko/diff/in_*_11x.lvl --gamefile _out/run/soko/diff/in_*_11x.game --tileset _out/run/soko/diff/setup_ts.tileset --text-key-only
+python sturgeon/input2tile.py --outfile _out/run/soko/diff/setup_19x.tile --textfile _out/run/soko/diff/in_*_19x.lvl --gamefile _out/run/soko/diff/in_*_19x.game --tileset _out/run/soko/diff/setup_ts.tileset --text-key-only
 
-python sturgeon/tile2scheme.py --outfile _out/run/soko/diff/setup_P_6x.scheme --tilefile _out/run/soko/diff/setup_6x.tile --pattern 0=nbr-plus 2=nbr-plus X=single
-python sturgeon/tile2scheme.py --outfile _out/run/soko/diff/setup_P_7x.scheme --tilefile _out/run/soko/diff/setup_7x.tile --pattern 0=nbr-plus 2=nbr-plus X=single
+python sturgeon/tile2scheme.py --outfile _out/run/soko/diff/setup_P_11x.scheme --tilefile _out/run/soko/diff/setup_11x.tile --pattern 0=nbr-plus 2=nbr-plus X=single
+python sturgeon/tile2scheme.py --outfile _out/run/soko/diff/setup_P_19x.scheme --tilefile _out/run/soko/diff/setup_19x.tile --pattern 0=nbr-plus 2=nbr-plus X=single
 
-python sturgeon/scheme2merge.py --outfile _out/run/soko/diff/setup_P.scheme --schemefile _out/run/soko/diff/setup_P_6x.scheme _out/run/soko/diff/setup_P_7x.scheme
+python sturgeon/scheme2merge.py --outfile _out/run/soko/diff/setup_P.scheme --schemefile _out/run/soko/diff/setup_P_19x.scheme _out/run/soko/diff/setup_P_11x.scheme
 
-python sturgeon/tilediff2scheme.py --outfile _out/run/soko/diff/setup_D_6x.scheme --tilefile _out/run/soko/diff/setup_6x.tile --diff-offset-row 8 --game 1
-python sturgeon/tilediff2scheme.py --outfile _out/run/soko/diff/setup_D_7x.scheme --tilefile _out/run/soko/diff/setup_7x.tile --diff-offset-row 9 --game 1
+python sturgeon/tilediff2scheme.py --outfile _out/run/soko/diff/setup_D_11x.scheme --tilefile _out/run/soko/diff/setup_11x.tile --diff-offset-row 13 --game 1
+python sturgeon/tilediff2scheme.py --outfile _out/run/soko/diff/setup_D_19x.scheme --tilefile _out/run/soko/diff/setup_19x.tile --diff-offset-row 21 --game 1
 
 # remap scheme files to output height and merge
 
-python sturgeon/scheme2merge.py --outfile _out/run/soko/diff/setup_D_9x-A.scheme --schemefile _out/run/soko/diff/setup_D_6x.scheme --remap-row " -10,-6=-3" " -2,2=0" "6,10=3"
-python sturgeon/scheme2merge.py --outfile _out/run/soko/diff/setup_D_9x-B.scheme --schemefile _out/run/soko/diff/setup_D_7x.scheme --remap-row " -11,-7=-2" " -2,2=0" "7,11=2"
+python sturgeon/scheme2merge.py --outfile _out/run/soko/diff/setup_D_9x-B.scheme --schemefile _out/run/soko/diff/setup_D_11x.scheme --remap-row " -15,-11=2" " -2,2=0" "11,15=-2"
+python sturgeon/scheme2merge.py --outfile _out/run/soko/diff/setup_D_9x-A.scheme --schemefile _out/run/soko/diff/setup_D_19x.scheme --remap-row " -23,-19=10" " -2,2=0" "19,23=-10"
 python sturgeon/scheme2merge.py --outfile _out/run/soko/diff/setup_9x.scheme --schemefile _out/run/soko/diff/setup_P.scheme _out/run/soko/diff/setup_D_9x-A.scheme _out/run/soko/diff/setup_D_9x-B.scheme
 
 # create tag file and text constraint
